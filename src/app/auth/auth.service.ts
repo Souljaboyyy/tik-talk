04039b13a07a4fa53d0e08e6f82ae608dece27ba
Makar Karpovski <makarkarpovski@mail.ru>
2025-10-19 18:31:22 +0500

    feat: Улчшена авторизация, добавлена работа с cookies, добавлен interceptor, guard, улучшен роутинг и работа с токенами

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,71 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { TokenResponse } from './auth.interface';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  http = inject(HttpClient);
-  baseApiUrl :string = 'https://icherniakov.ru/yt-course/auth/';
+  http = inject(HttpClient)
+  cookieService = inject(CookieService);
+  router = inject(Router);
+  baseApiUrl :string = 'https://icherniakov.ru/yt-course/auth/'
 
-  login(payload: { username: string, password: string }){
-    const fd = new FormData();
-    fd.append('username', payload.username);
-    fd.append('password', payload.password);
+  token: string | null = null;
+  refreshToken: string | null = null;
 
-    return this.http.post(
+  get isAuth(){
+    if(!this.token){
+      this.token = this.cookieService.get('token')
+    }
+
+    return !!this.token;
+  }
+
+  login(payload: {username: string, password: string}){
+    const fd = new FormData()
+
+    fd.append('username', payload.username)
+    fd.append('password', payload.password)
+
+    return this.http.post<TokenResponse>(
       `${this.baseApiUrl}token`,
-      fd
-    );
+      fd,
+    ).pipe(
+      tap(val => this.saveTokens(val))
+    )
+  }
+
+  refreshAuthToken(){
+    return this.http.post<TokenResponse>(
+      `${this.baseApiUrl}token`,
+      {
+        refreshToken: this.refreshToken
+      }
+    ).pipe(
+      tap(val => this.saveTokens(val)),
+      catchError(err => {
+        this.logout()
+        return throwError(err)
+      })
+    )
+  }
+
+  logout(){
+    this.cookieService.deleteAll()
+    this.token = null
+    this.refreshToken = null
+    this.router.navigate(['/login'])
+  }
+
+  saveTokens(res: TokenResponse){
+    this.token = res.access_token
+    this.refreshToken = res.refresh_token
+
+    this.cookieService.set('token', this.token)
+    this.cookieService.set('refreshToken', this.refreshToken)
   }
 }
